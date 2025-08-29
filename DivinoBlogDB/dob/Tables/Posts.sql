e6d8c4615fb4111a4a44dd167d1d1ddd9d0f1412
Vincent Perry Divino <156486099+vpmdivino@users.noreply.github.com>
2025-08-29 17:46:39 +0800

    Add files via upload

---
+++ DivinoBlogDB/dob/Tables/Posts.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Posts]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [UserID] INT NOT NULL, 
+    [Title] NVARCHAR(150) NOT NULL, 
+    [Body] TEXT NOT NULL, 
+    [DateCreated] DATETIME2 NOT NULL,
+    CONSTRAINT [FK_Posts_Users] FOREIGN KEY (UserID) REFERENCES Users(Id)
+)
