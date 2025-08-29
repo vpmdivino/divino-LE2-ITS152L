754ba22a1066dc0f5cb233558b96fd4b439080a2
Vincent Perry Divino <156486099+vpmdivino@users.noreply.github.com>
2025-08-29 17:47:59 +0800

    Add files via upload

---
+++ BlogDataLibrary/Models/ListPostModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogDataLibrary.Models
+{
+    public class ListPostModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
